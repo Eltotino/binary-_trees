b395ee3caa5c0f8e6fbf4eaabb068edb77286224
Eltotino <totojaber1@gmail.com>
2021-05-17 15:46:07 +0100

    root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-*binary_tree_is_leaf - check if a node is a root
+*binary_tree_is_root - check if a node is a root
 *@node: root node
 *
 * Return: 1 on success or 0 on failure
@@ -10,7 +10,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if(node->parent == NULL)
+	if (node->parent == NULL)
 		return (1);
 	else
 		return (0);
