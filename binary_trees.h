7e04ce72257c486810e4280e568ff1a4137907ba
Eltotino <totojaber1@gmail.com>
2021-07-11 11:50:28 +0100

    level

--- binary_trees.h
+++ binary_trees.h
@@ -15,16 +15,32 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+/**
+ * struct queue_s - doubly linked list representation of a stack (or queue)
+ * @node: a pointer to a binary tree node
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct queue_s
+{
+const binary_tree_t *node;
+struct queue_s *prev;
+struct queue_s *next;
+} queue_t;
 
 /*Prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -50,7 +66,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t
 *binary_trees_ancestor(const binary_tree_t *first,
-                        const binary_tree_t *second);
+const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
